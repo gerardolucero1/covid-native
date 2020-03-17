b4f6a0a09dbbf3faf5f77b668d57318dcfb56b58
Gerardo Lucero <34350583+gerardolucero1@users.noreply.github.com>
2020-03-17 15:46:32 -0700

    genero la estructura del proyecto

---
+++ app/components/pages/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <Page loaded="onLoaded" navigatedTo="onNavigatedTo">
+        <GridLayout rows="*, *" colums="*">
+            <StackLayout row="1" col="1" justifyContent="center" alignItems="center">
+                <Label text="Login" textWrap="true" />
+                
+            </StackLayout>
+        </GridLayout>
+    </Page>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
